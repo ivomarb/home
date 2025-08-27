fa9ef00fc00df7e0a10793cb21e9642ae1e20fe9
Ivomar Brito Soares <ivomarbsoares@gmail.com>
2025-08-27 11:52:10 -0300

    Updates

---
+++ precache-manifest.a9a4e49e4be2b098d97216d29ae5d1d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6266f01c243c58e54c602a6bbc56ebdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e4dbedcc5c77d45737",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "982e4ef363714728a379",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "a4e4dbedcc5c77d45737",
+    "url": "/static/js/2.9c4c71ed.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.9c4c71ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982e4ef363714728a379",
+    "url": "/static/js/main.95d1e179.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "f3e984e2f443d7a1ee3d04497e493a34",
+    "url": "/static/media/airtop-logo.f3e984e2.png"
+  },
+  {
+    "revision": "93ee39eec022779291c22c0dfd6f8de6",
+    "url": "/static/media/cgdatascience-logo.93ee39ee.jpg"
+  },
+  {
+    "revision": "fd650cd7dfe3d511e01dedcf4c6aea36",
+    "url": "/static/media/resume.fd650cd7.pdf"
+  },
+  {
+    "revision": "f8ba65f34ffd6fb9ea6e65c5bd75723f",
+    "url": "/static/media/scikit-learn-logo.f8ba65f3.png"
+  }
+]);
